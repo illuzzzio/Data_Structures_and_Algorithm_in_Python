315782b2e8f0af1cf7053c1e6a040322eddb54a9
Pranjal Malhotra <152037759+illuzzzio@users.noreply.github.com>
2026-07-19 00:42:09 +0530

    importance of else statement

---
+++ neetcode_150/leetcode_11_container_with_most_water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0 
+        right = len(height)-1
+        max_area = float("-inf")
+
+        while(right>left):
+            area = min(height[left],height[right])*(right-left)
+            max_area = max(area,max_area)
+            if(height[left]<height[right]):
+                left+=1
+            else:
+                right -=1
+
+        return max_area
