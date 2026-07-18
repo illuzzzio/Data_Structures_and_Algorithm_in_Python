cd0b66f672c726cb444f7a5a39e83e19d4111653
Pranjal Malhotra <152037759+illuzzzio@users.noreply.github.com>
2026-07-18 23:31:21 +0530

    optimal

--- sliding_window/max_subarray_product_size_k.py
+++ sliding_window/max_subarray_product_size_k.py
@@ -12,3 +12,18 @@ def product(arr,k):
     return max_sum
 print(product(arr,k))
         
+
+def optimal(arr,k):
+    current_product = 1
+    for i in range(0,k):
+        current_product +=arr[i]
+    max_product = current_product 
+    for i in range(len(arr)-k):
+        current_product /=arr[i]
+        current_product *= arr[i+k]\
+        
+        if(current_product>max_product):
+            max_product = current_product
+    return max_product 
+print(product(arr,k))
+
