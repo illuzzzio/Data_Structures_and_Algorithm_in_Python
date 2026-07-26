26f957680b6326e5a25127f8e7bf0723cb10cc1c
Pranjal Malhotra <152037759+illuzzzio@users.noreply.github.com>
2026-07-26 23:47:59 +0530

    threesome

--- neetcode_150/leetcode_15_3_sum.py
+++ neetcode_150/leetcode_15_3_sum.py
@@ -1,37 +1,27 @@
 class Solution:
-    def threeSum(self, nums):
-        nums.sort()
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         result = []
-
-        for i in range(len(nums) - 2):
-
-            # Skip duplicate first elements
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
-            left = i + 1
-            right = len(nums) - 1
-
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total < 0:
-                    left += 1
-
-                elif total > 0:
-                    right -= 1
-
+        nums.sort()
+        for i,v in enumerate(nums):
+            if(i>0 and v==nums[i-1]):
+                continue 
+            left,right = i+1,len(nums)-1
+            while(right>left):
+                threesum = (v+nums[left]+nums[right])
+                if(threesum>0):
+                    right-=1
+                elif(threesum<0):
+                    left+=1
                 else:
-                    result.append([nums[i], nums[left], nums[right]])
+                    result.append([v,nums[left],nums[right]])
+                    left+=1
+                    right-=1
 
-                    left += 1
-                    right -= 1
-
-                    # Skip duplicates
+                    # skip duplicates 
+                    
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
 
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
-
-        return result
+        return result 
